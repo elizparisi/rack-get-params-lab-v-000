31715586715d656a1bf3a9616fcfabea862f76b6
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-15 00:53:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,8 @@ class Application
     elsif req.path.match(/cart/)
       if cart.empty? 
         resp.write "Your cart is empty"
-      else 
+      else cart.each do |item|
+        
     else
       resp.write "Path Not Found"
     end
