93439a8b101eab151c8c279937f473c71e596265
Elizabeth Parisi <parisielizabeth@gmail.com>
2019-06-15 01:01:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,8 @@ class Application
       end
     end
     elsif req.path.match(/add/)
-      
+      search_term = req.params["item"]
+        
     else
       resp.write "Path Not Found"
     end
